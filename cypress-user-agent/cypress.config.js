f50266e237761f0078bfe7f9ded46beb97cacffc
jjhelguero <jjhelguero@protonmail.com>
2022-09-13 11:41:46 -0500

    add userAgent

--- cypress-user-agent/cypress.config.js
+++ cypress-user-agent/cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    userAgent:
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
   },
 });
